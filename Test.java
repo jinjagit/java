9e7f64e2bb95ae2559511a06d4a2ef376949054c
Simon Tharby <simon@liquidvoting.io>
2021-06-10 22:19:54 +0100

    minor corrections

--- Test.java
+++ Test.java
@@ -29,7 +29,7 @@ public class Test {
       System.out.println();
       System.out.println("Average of the numbers in the array: " + averageOfArray(arr));
       System.out.println("Highest of the numbers is: " + arr[0]);
-      System.out.println("Lowest of the numbers is : " + arr[arr.length - 1]);
+      System.out.println("Lowest of the numbers is : " + arr[n - 1]);
     }
   }
 
